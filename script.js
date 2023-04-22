d05975206dcd9bb0fca400dfb64c53013292e35e
TUAL KHAN MUAN <andrewtualmuan@gmail.com>
2023-04-22 22:45:21 +0630

    major changes

--- script.js
+++ script.js
@@ -1,3 +1,39 @@
+// Get the current date
+// Get the current date object
+const currentDate = new Date();
+
+// Define an array of month names
+const monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+
+// Get the day, month, and year from the current date object
+const day = currentDate.getDate();
+const monthIndex = currentDate.getMonth();
+const year = currentDate.getFullYear();
+
+// Get the month name from the array using the month index
+const monthName = monthNames[monthIndex];
+
+// Construct the date string in the format "dd MMM, yyyy"
+const dateString = `${day} ${monthName}, ${year}`;
+
+// Display the current date string in the HTML element with id "dateDisplay"
+document.getElementById("dateDisplay").textContent = "Current Date: " + dateString;
+
+
+// JavaScript code to animate the website title
+var titleElement = document.getElementById('website-title');
+var translateY = -20;
+
+function animateTitle() {
+    titleElement.style.transform = 'translate(-50%, -50%) translateY(' + translateY + 'px)';
+    translateY = -translateY;
+    setTimeout(function() {
+        animateTitle();
+    }, 2000);
+}
+
+animateTitle();
+
 //slideshow of Certificates
 
 var certificates = [
