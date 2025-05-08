8949bd1f93d213f3372577d2d2bfe1b3168e18e8
TUALMUAN <116189303+TUALMUAN@users.noreply.github.com>
2025-05-08 15:08:05 +0630

    photos

--- script.js
+++ script.js
@@ -41,8 +41,7 @@ document.getElementById("dateDisplay").textContent = "Last Updated Date: " + dat
 // JavaScript code to animate the website title
 
 var myPhotos = [
-  "https://raw.githubusercontent.com/TUALMUAN/Web-Images/main/2025.2.15_Photo%201.jpg",
-  "https://raw.githubusercontent.com/TUALMUAN/Web-Images/main/2025.2.15_Photo%203.jpg",
+
   "https://github.com/TUALMUAN/Web-Images/blob/main/2025.2.15_Photo%204.jpg?raw=true",
    ];
    
